fe0adc0ef4facabaa199cc027ddd9d7016ff7f60
Del Dewar <del.dewar@gmail.com>
2026-02-26 16:56:51 +0000

    Change Region cancel timeout to 5s

--- src/App.tsx
+++ src/App.tsx
@@ -268,7 +268,7 @@ function App() {
           await new Promise((resolve) => window.setTimeout(resolve, 120));
 
           const snipPath = await invoke<string | null>("capture_windows_snip_to_file", {
-            timeoutMs: 15_000,
+            timeoutMs: 5_000,
           });
 
           if (snipPath && sessionRef.current) {
